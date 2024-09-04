ab60f105691472b2992d19f8843136b4f6fa7e0e
Nitipoom1456 <nitipoom.sut@odds.team>
2024-09-05 02:10:47 +0700

    add simple show user detail page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -11,7 +11,7 @@ class UserController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path, notice: 'User was successfully created.'
+      redirect_to root_path
     else
       render :new
     end
@@ -22,9 +22,9 @@ class UserController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to user_index_path
+      redirect_to root_path
     else
-      render 'edit'
+      render :edit
     end
   end
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get "/register", to: "user#new"
   post "/register", to: "user#create"
   # get "/user/:id", to: "user#show",
-  # get "/user/:id/edit", to: "user#edit",
+  # get "/user/:id/edit", to: "user#edit"
   # patch "/user/:id", to: "user#update"
-  delete "/user/:id", to: "user#destroy"
+  # delete "/user/:id", to: "user#destroy"
 end
