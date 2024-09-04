b98374c83a2af817e488abf8301c60f78e40a004
Nitipoom1456 <nitipoom.sut@odds.team>
2024-09-04 21:38:34 +0700

    user list page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root "user#index"
+  # get "users/new" => "users#new", as: :new_user
+  # post "users" => "users#create"
+  # get "users/:id" => "users#show", as: :user
+  # get "users/:id/edit" => "users#edit", as: :edit_user
+  # patch "users/:id" => "users#update"
+  # delete "users/:id" => "users#destroy"
 end
